5c7ce61be6d6af554fad8c4cc7bda40d37a0bab8
BrieBLee <boram1073@gmail.com>
2020-09-05 02:28:36 +0900

    Updates

---
+++ precache-manifest.cdb9b893c57b94ecf2fb0d74b2ed10f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3a84e824e0bc145965eb2a8ecfc811",
+    "url": "/movie_2020_08/index.html"
+  },
+  {
+    "revision": "9a6b78fd50a6c05a7eea",
+    "url": "/movie_2020_08/static/css/main.0cac5a52.chunk.css"
+  },
+  {
+    "revision": "d77f9569666240754311",
+    "url": "/movie_2020_08/static/js/2.ec981d1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_2020_08/static/js/2.ec981d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a6b78fd50a6c05a7eea",
+    "url": "/movie_2020_08/static/js/main.189ea9be.chunk.js"
+  },
+  {
+    "revision": "0cc17c23b3c3e428d96d",
+    "url": "/movie_2020_08/static/js/runtime-main.42af3cdc.js"
+  }
+]);
